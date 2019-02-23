0f832cc77227e48d1bd9b1c9a7092d71c17dbcfe
turingvsclarke <rhinoeades@gmail.com>
2019-02-22 22:29:22 -0500

    Consolidating files

--- ChangeMaker/ChangeMakerBB.py
+++ ChangeMaker/ChangeMakerBB.py
@@ -1,170 +1,55 @@
-# VENDING MACHINE that allows the user to select and purchase several different entries and then runs the change-making process
-# This could be modified for any number of products and corresponding prices, as long as they are added to the respective lists at the same index
+# A program that computes how much change you owe after you pay for something
 
-# Print all the products available and their corresponding prices. Ask which product they want
+# Take in the price of the item as before, multiplied by 100
 
-products = ["laptop","cheeseburger","apple","phone","crackers","chips","candy bar","cookies","lollipop","limousine"]
+purchase_price = float(input("How much did the item cost?\n"))*1000
 
-product_cost = [495,2.40,1.19,240,3.15,1.50,2.18,1.19,1.20,20000]
+# Ask for the amount of cash tendered, multiply it by 100
 
-keep_running = True
+cash_tendered = (float(input("How much cash was paid?\n"))*1000)
 
-# Initiate variables so they don't have to be initiated within a conditional loop
+# Calculate the change in pennies. Note that this assumes they paid enough money
 
-selectionList = []
+change = cash_tendered - purchase_price
 
-purchase_price = 0
+# Quantitative denominations list and singular/plural denominations descriptors
 
-cash_tendered = 0
+denominations_list = [20000,10000,5000,1000,250,100,50,10]
 
-product_number = 1
+denominations_plural = ["twenties","tens","fives","ones","quarters","dimes","nickels","pennies"]
 
+denominations_single = ["twenty","ten","five","one","quarter","dime","nickel","penny"]
 
-while keep_running:
+# Tell the user that the amount of change will soon be outputed 
 
-    # Begin the program with an introduction to how the user should interact with the code
+print("The change is as follows:")
 
-    print("Enter the numbers of each corresponding entry you want and hit ENTER after each one. Type FINISH when finished:")
+# Create a for loop that removes multiples of the denominations for each one in our list
 
-    # Print and number all the available products
+for x in denominations_list:
 
-    for x in products:
-        
-        print(str(product_number) + ". " + x.capitalize() + " ",product_cost[products.index(x)])
+    # Use the same modular formula as before
 
-        # Create a string of all acceptable numbers
+    denomination_quantity = int((change-(change%x))/x)
 
-        acceptable_products = "" + str(product_number)
+    change = change%x
 
-        # Assign the next product the next number
+    # Store each result in a string of change denominations, as long as it's non-empty
 
-        product_number = product_number + 1
-        
-    # Accept the number of the product that the user wants
+    if denomination_quantity!=0:
 
-    selection = input()
+        # Use singular denominations list for singular change
 
-    # Keep accepting new selections until the user enters the command to terminate the entering process
+        if denomination_quantity==1:
 
-    while selection!="FINISH" and selection!="finish":
+            # Print number and type of each denomination
 
-        # checks to make sure that the product selected is a valid product, in the range of the products
+            print(str(denomination_quantity) + " " + denominations_single[denominations_list.index(x)] + ".")
 
-        while selection.replace(" ","") not in acceptable_products:
+        # Use plural denominations list for plural change
 
-            print("I'm sorry. That's not a valid item. Please try again.")
+        else:
 
-            selection = input()
+            # Print number and type of each denomination
 
-        # Take input every time the input isn't finished    
-
-        selected_product = int(selection)
-
-        # adds the item to the user's list of products that they want to buy
-
-        selectionList.append(selected_product)
-
-        # takes new input
-
-        selection = input()
-
-    # Get a total price, matching each product with its corresponding cost in the cost list
-
-    for y in selectionList:
-
-        # add all the costs of the products together, rounding to two digits to avoid floating point issues 
-
-        purchase_price = round((purchase_price) + (product_cost[y - 1]),2)
-
-    # prints out how much the total cost is
-
-    cash_tendered = float(input("The total is " + str(purchase_price) + ". Please enter how much you are going to pay as a decimal number: dollars.cents\n"))
-
-    # Checks to make sure that the amount entered is at least as much as the cost
-
-    while cash_tendered < purchase_price:
-
-        # Ask the user for more money if they haven't paid enough money yet
-
-        new_cash = round(input("You owe " + str(purchase_price - cash_tendered) + ". Please enter more money.\n"),2)
-
-        # Accumulate the total amount paid
-
-        cash_tendered = new_cash + cash_tendered
-
-    # Print a receipt, including the tender history and products purchased
-
-    print("Items purchased:")
-
-    # print the name of every item purchased
-
-    for z in selectionList:
-
-        print(products[z-1].capitalize() + "                                          ",product_cost[z-1])    
-
-    # Print the total cost and the paid amount
-
-    print("Total: ",purchase_price)
-    print("Cash tendered: ",cash_tendered)
-
-    # Run the basic process to compute change
-
-    # Take in the price of the item as before, multiplied by 100 to work in pennies
-
-    price = float((purchase_price)*100)
-
-    # Ask for the amount of cash tendered, multiply it by 100 to work in pennies
-
-    cash = float((cash_tendered)*100)
-
-    # compute the gross change
-
-    change = cash - price
-
-    # Let the user see how much change there is
-
-    print("Change: ",change)
-
-    # List for every denomination in English as singular and plural and quantitatively
-
-    denominations_list = [2000,1000,500,100,25,10,5,1]
-
-    denominations_plural = ["twenties","tens","fives","ones","quarters","dimes","nickels","pennies"]
-
-    denominations_single = ["twenty","ten","five","one","quarter","dime","nickel","penny"]
-
-    # Only run the operations to get change if there actually is change
-
-    if change != 0:
-
-        print("The change is as follows:")
-
-        # Create a for loop that computes the amount of each denomination in the change using modular arithmetic
-
-        for x in denominations_list:
-
-            # Use the same modular formula as before
-
-            denomination_quantity = int((change-(change%x))/x)
-
-            change = change%x
-
-            # Store each result in a string of change denominations, as long as it's non-empty
-
-            if denomination_quantity!=0:
-
-                # Use singular denominations list for singular change
-
-                if denomination_quantity==1:
-
-                        print(str(denomination_quantity) + " " + denominations_single[denominations_list.index(x)] + ".")
-
-                # Use plural denominations list for plural change
-
-                else:
-
-                    print(str(denomination_quantity)+ " " + denominations_plural[denominations_list.index(x)]+".")
-
-    # Print a receipt that has each item purchased, its price, total price, cash tendered, change back
-
-    keep_running = False
+            print(str(denomination_quantity)+ " " + denominations_plural[denominations_list.index(x)]+".")
